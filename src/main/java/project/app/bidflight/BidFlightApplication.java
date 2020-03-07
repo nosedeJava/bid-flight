55dc3ceaadc28d62763ec89514c2672536f7c6e2
urobs17 <michael.preciado@mail.escuelaing.edu.co>
2020-03-06 19:38:14 -0500

    spring initialzr

---
+++ src/main/java/project/app/bidflight/BidFlightApplication.java
@@ -0,0 +1,13 @@
+package project.app.bidflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidFlightApplication.class, args);
+	}
+
+}
