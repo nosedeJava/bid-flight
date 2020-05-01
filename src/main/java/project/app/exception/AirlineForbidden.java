2e24492b0a4306e64e8562c628a20c653103acd6
Stilink <stilink306@gmail.com>
2020-04-30 21:47:55 -0500

    Filtro de take off date - Avance en el back para las pujas

--- src/main/java/project/app/exception/AirlineForbidden.java
+++ src/main/java/project/app/exception/AirlineForbidden.java
@@ -9,7 +9,7 @@ public class AirlineForbidden extends Exception{
      *
      */
     private static final long serialVersionUID = -4462433087135243852L;
-
+    
     public AirlineForbidden(String message) {
         super(message);
     }
--- src/main/java/project/app/realtime/BidRealtimeHandler.java
+++ src/main/java/project/app/realtime/BidRealtimeHandler.java
@@ -1,25 +1,49 @@
 package project.app.realtime;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import project.app.exception.AuctionNotFound;
+import project.app.exception.BidderNotFound;
+import project.app.exception.InconsistentBid;
 import project.app.model.Bid;
-
-
+import project.app.services.AuctionServices;
+import project.app.services.BidderServices;
 
 @Controller
 public class BidRealtimeHandler {
-	
+
 	@Autowired
 	SimpMessagingTemplate msgt;
-    
-	@MessageMapping("/auctions/{idAuction}")    
-	public void handleBidEvent(Bid bid, @DestinationVariable int idAuction) throws Exception {
-		System.out.println("Nuevo bid recibido en el servidor!: "+idAuction+" "+bid.toString());
-		msgt.convertAndSend("/topic/auctions/"+idAuction, bid);
+
+	@Autowired
+	AuctionServices auctionServices;
+
+	@Autowired
+	BidderServices bidderServices;
+
+	@MessageMapping("/auctions/{idAuction}")
+	public void handleBidEvent(Bid bid, @DestinationVariable int idAuction) {
+		System.out.println("Bid recibida en el servidor: " + bid.toString());
+		try {
+			if (!bidderServices.validateBid(bid)) {
+				msgt.convertAndSend("/app/auctions/" + idAuction, new InconsistentBid("Bid inconsistente - Saldo insuficiente"));
+			} else {
+				auctionServices.updateAuction(bid);
+				msgt.convertAndSend("/app/auctions/"+idAuction, bid);
+			}
+		} catch (BidderNotFound e1) {
+			msgt.convertAndSend("/app/auctions/" + idAuction, e1);
+			e1.printStackTrace();
+		} catch (AuctionNotFound e) {
+			msgt.convertAndSend("/app/auctions/" + idAuction, e);
+			e.printStackTrace();
+		} catch (InconsistentBid e) {
+			msgt.convertAndSend("/app/auctions/" + idAuction, e);
+			e.printStackTrace();
+		}
 	}
 }
